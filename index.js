e358bc41b8bf4222477d8710a8967fea9fc33c2c
Wasim <princewasim971@gmail.com>
2024-10-03 12:08:53 -0700

    update json file

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 import config from "./config/index.js";
 import connectDB from "./config/db.js";
-
 import app from "./app.js";
 
+// Connect to the database
 connectDB();
 
-const port = config.port || 4000;
-
-app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
-});
-
-
-
-// import config from "./config/index.js";
-// import connectDB from "./config/db.js";
-// import app from "./app.js";
-
-// // Connect to the database
-// connectDB();
-
-// // Instead of listening on a port, export the app
-// export default app;
+// Instead of listening on a port, export the app
+export default app;
